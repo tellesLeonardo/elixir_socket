92ae56ee4f57530257ee00bb7b65a4e93a54a4e5
@gabriel96 <gabriel@sac.digital>
2021-03-04 17:30:32 -0300

    att

--- lib/socket_v1/socket_handler.ex
+++ lib/socket_v1/socket_handler.ex
@@ -2,49 +2,64 @@ defmodule SocketV1.SocketHandler do
 
   @behaviour :cowboy_websocket
 
-  def init(request, _state) do
-    request |> IO.inspect
-
-    # [_elem1, number] = request.path_info
-    state = %{registry_key: request.path,number: "number", pid: request.pid }
-
-    {:cowboy_websocket, request, state, %{ :idle_timeout => 500_000_000_000, :inactivity_timeout => 500_000_000_000 } }
-
+  def init(req, state) do
+    IO.puts "modo avião"
+    {:cowboy_websocket, req, state, %{ :idle_timeout => 500_000_000_000, :inactivity_timeout => 500_000_000_000,:max_connections =>  :infinity} }
   end
 
   def websocket_init(state) do
-
-    state |> IO.inspect
-
+    IO.puts "websocket_init websocket_init"
+    # state |> IO.inspect
     # {:ok, _} = Registry.register(Registry.SocketV1, state.number, state)
 
     {:ok, state}
   end
 
   def websocket_handle({:text, json}, state) do
-    payload = Jason.decode!(json)
-    message = payload["data"]["message"]
+    payload = Jason.decode!(json, keys: :atoms)
 
-    # Registry.SocketV1
-    # |> Registry.dispatch(state.number, fn(entries) ->
-    #   [{pid, _value}] = entries
-    #     pid |> IO.inspect
-    # #   for {pid, _} <- entries do
-    # #     if pid != self() do
-    #       Process.send(pid, message, [])
-    # #     end
-    # #   end
-    # end)
+    (payload.event, payload)
 
-    {:reply, {:text, message}, state}
+
+    |> IO.inspect
+
+    IO.puts "bondae"
+    :timer.sleep(100_000)
+    device = payload["command"]
+
+    Registry.SocketV1
+    |> Registry.register(device, {})
+
+    {:reply, {:text, %{status: true, message: "authenticated"} |> Jason.encode!}, state}
   end
+
+
+  defp handling_events() do
+
+  end
+
+  # def websocket_handle({:text, json}, state) do
+  #   payload = Jason.decode!(json)
+  #   message = payload["data"]["message"]
+  #   # Registry.SocketV1
+  #   # |> Registry.dispatch(state.registry_key, fn(entries) ->
+  #   #   for {pid, _} <- entries do
+  #   #     if pid != self() do
+  #   #       Process.send(pid, message, [])
+  #   #     end
+  #   #   end
+  #   # end)AppMon
+
+  #   # %{url_media: "",url_contacts: "", status: }
+
+  #   {:reply, {:text, "message"}, state}
+  # end
+
+
 
   def websocket_info(info, state) do
+    IO.puts "websocket_info "
     {:reply, {:text, info}, state}
-  end
-
-  def via_tupla(id)  do
-    {:via, Registry, {:number_register, id}}
   end
 
 end
